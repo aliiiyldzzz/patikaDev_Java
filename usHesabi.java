7c545dab4c3aa0a94b5e7e78f69830cda70c25a8
ALİ YILDIZ <56775023+aliiiyldzzz@users.noreply.github.com>
2023-09-12 14:39:19 +0300

    Add files via upload

---
+++ usHesabi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class usHesabi {
+    static int power(int base,int exponent) {
+        Scanner scan = new Scanner(System.in);
+        int result=1;
+        if (base == 1 || exponent == 0) {
+            return result;
+        }
+        result=base*power(base, exponent-1);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Üs değeri giriniz :");
+        int exponent = scan.nextInt();
+        System.out.println("Sonuç = " + power(base,exponent));
+
+    }
+
+}
